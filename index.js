8d9ffa38e594161a1b1720ba24cbc368776d8f71
MrSlimeDiamond <findlayrichardson3@gmail.com>
2021-03-07 21:09:38 +1300

    Fix multiple commands, add firstseen command with caching (until reboot)

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const irc = require("irc");
+const NodeCache = require("node-cache");
+const firstseen_cache = new NodeCache();
 const request = require("request");
 const cheerio = require("cheerio");
 const client = new irc.Client('irc.esper.net', "NarwhalBot-recode", {
@@ -30,11 +32,65 @@ client.addListener("message", async function(from, to, message){
         client.say(to, from+": Pong!");
     }
 
-    if(message.startsWith("!tl", "!!timeplayed")){
+    if(message.startsWith("!tl") || message.startsWith("!!timeplayed")){
         correctname = await getMcoAPI("getcorrectname", args[1]);
         hourslogged1 = await getMcoAPI("gettimeonline", correctname);
         hourslogged = Math.floor(hourslogged1 / 3600);
         client.say(to, args[1] + " has logged " + hourslogged + " hours on minecraftonline.com");
+        return;
+    }
+
+    if(message.startsWith("!fs") || message.startsWith("!!firstseen")){
+        correctname = await getMcoAPI("getcorrectname", args[1]);
+        fsc = firstseen_cache.get(correctname)
+        console.log("CN: "+correctname);
+        console.log(args[1])
+        console.log(fsc);
+
+        if(fsc == undefined){
+
+        var time = await getMcoAPI("getfirstseen_unix", correctname);
+        console.log(time.toString());
+        if(time.replace(/\n/g, '') == "INVALID" || time.replace(/\n/g, '') == "NOTFOUND"){
+            client.say(to, "Unknown username.")
+            return;
+        } else if(time.replace(/\n/g, '') == "UNKNOWN"){
+            client.say(to, "User's first seen date is unknown");
+            return;
+        }
+
+        epochnum = parseInt(time);
+        time1 = new Date(epochnum * 1000);
+        timenow = new Date();
+
+        var Difference_In_Time = timenow.getTime() - time1.getTime();
+   
+        var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24); 
+
+        ago = time1.toGMTString()
+
+        //correctname = await getMcoAPI("getcorrectname", args);
+        var stringToSay = correctname+"first logged into minecraftonline.com on " + time1.toGMTString() + ` (${Math.floor(Difference_In_Days)} days ago)`
+        client.say(to, stringToSay.replace(/\n/g, ' '));
+
+        firstseen_cache.set(correctname, epochnum);
+        client.say(to, "Caching firstseen result for "+correctname);
+        return;
+    }else{
+        time1 = new Date(fsc * 1000);
+        timenow = new Date();
+
+        var Difference_In_Time = timenow.getTime() - time1.getTime();
+   
+        var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24); 
+
+        ago = time1.toGMTString()
+
+        //correctname = await getMcoAPI("getcorrectname", args);
+        var stringToSay = correctname+"first logged into minecraftonline.com on " + time1.toGMTString() + ` (${Math.floor(Difference_In_Days)} days ago) (result from cache)`
+        client.say(to, stringToSay.replace(/\n/g, ' '));
+    }
+
         return;
     }
 } catch(err) {
